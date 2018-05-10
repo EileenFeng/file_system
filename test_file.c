959b77a2d63cf6c1073776fe7aa28fd5a14a563c
EileenFeng <efeng@brynmawr.edu>
2018-05-10 00:01:24 -0400

    ???

--- test_file.c
+++ test_file.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <assert.h>
 #include "file_lib.h"
 #include "fs_struct.h"
 #define WRITEBLOCK 17000
@@ -11,7 +12,24 @@ int main() {
 	f_seek(cur_file, 0, SEEKSET);
 	char* buffer=malloc(WRITEBYTE);
 	bzero(buffer, WRITEBYTE);
+	for(int i = 0; i < WRITEBYTE; i++) {
+	  buffer[i] = 'a';
+	}
 	f_write((void*)buffer, WRITEBYTE, cur_file);
+	bzero(buffer, WRITEBYTE);
+	f_read((void*)buffer, WRITEBYTE, cur_file);
+	int counter = 0;
+	for(int i = 0; i < WRITEBYTE; i++) {
+	  //printf("%d:  %c \t", i, buffer[i]);
+	  if(buffer[i] != 'a') {
+	    counter ++;
+	  }
+	  //assert(buffer[i] == 'a');
+	  
+	}
+	
 	f_close(cur_file);
+	free(buffer);
 	f_remove("home/feichi/large",0);
+	printf("counter hehe %d\n", counter);
 }
